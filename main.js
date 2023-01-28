0e2c9de780bbf87cc378e55affc07822b9977a5b
Philippe Victoria <vp8@mbp-de-philippe.home>
2023-01-28 17:05:22 +0100

    modif url vers northflank et ajout script main.js dans index.html

--- main.js
+++ main.js
@@ -5,18 +5,22 @@ document.addEventListener("DOMContentLoaded", () => {
     .querySelector("#contactForm")
     .addEventListener("submit", async (e) => {
       e.preventDefault();
+      try {
+        const data = {
+          firstname: document.querySelector("#firstname").value,
+          lastname: document.querySelector("#lastname").value,
+          email: document.querySelector("#email").value,
+          subject: document.querySelector("#subject").value,
+          message: document.querySelector("#message").value,
+        };
 
-      const data = {
-        firstname: document.querySelector("#firstname").value,
-        lastname: document.querySelector("#lastname").value,
-        email: document.querySelector("#email").value,
-        subject: document.querySelector("#subject").value,
-        message: document.querySelector("#message").value,
-      };
-
-      /* console.log(data); */
-
-      const response = await axios.post("http://localhost:3000/form", data);
-      console.log(response);
+        const response = await axios.post(
+          "https://site--back-end-formulaire--nm6dw4wybf2m.code.run/",
+          data
+        );
+        res.status(200).json(response);
+      } catch (error) {
+        res.status(400).json({ message: "There is a problem" });
+      }
     });
 });
